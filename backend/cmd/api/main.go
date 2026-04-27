5c065ff532049666765f5552a44c977e019da1f5
Jaden M <jrmantie@hotmail.com>
2026-04-26 21:25:04 -0600

    add in database connection

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,12 +4,26 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/Mantie7553/MediaHub/backend/internal/database"
 	"github.com/Mantie7553/MediaHub/backend/internal/server"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := database.New()
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err)
+	}
+	defer db.Close()
+
+	fmt.Println("Database connected successfully")
+
 	s := server.New()
-	fmt.Println("MediaHub API starting on port 8080")
+	fmt.Println("MediaHub API starting on port 9090...")
 	if err := s.Start(":9090"); err != nil {
 		log.Fatal(err)
 	}
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,8 @@ module github.com/Mantie7553/MediaHub/backend
 
 go 1.26.2
 
-require github.com/go-chi/chi/v5 v5.2.5 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+)
